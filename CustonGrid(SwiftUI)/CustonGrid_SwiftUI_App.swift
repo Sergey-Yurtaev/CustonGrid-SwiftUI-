b2cfafb9c3e207891bd506d487ae65887f035ada
Sergey-Yurtaev <engineerk2@mail.ru>
2022-07-21 17:35:14 +0500

    Initial Commit

---
+++ CustonGrid(SwiftUI)/CustonGrid_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  CustonGrid_SwiftUI_App.swift
+//  CustonGrid(SwiftUI)
+//
+//  Created by  Sergey Yurtaev on 21.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CustonGrid_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
